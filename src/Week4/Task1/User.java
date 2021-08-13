0149ce5a7dff12d4311bd78fa23ae035138a68ef
anushikghazaryan <anushghazaryan.23@gmail.com>
2021-08-13 22:52:41 +0400

    week4 task1

---
+++ src/Week4/Task1/User.java
@@ -0,0 +1,60 @@
+package Week4.Task1;
+
+public class User implements Cloneable{
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(User user) {
+        if(user != null) {
+            this.firstName = user.getFirstName();
+            this.lastName = user.getLastName();
+            this.userName = user.getUserName();
+            this.email = user.getEmail();
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    protected User clone() throws CloneNotSupportedException {
+        return (User) super.clone();
+    }
+}
---
+++ src/Week4/Task1/UserGroup.java
@@ -0,0 +1,42 @@
+package Week4.Task1;
+
+public class UserGroup implements Cloneable{
+    private User[] users;
+    private int nextIndex;
+
+    public UserGroup() {
+        users = new User[10];
+        nextIndex = 0;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public void setUsers(User[] users) {
+        this.users = users;
+    }
+
+    public void addUser(User user) {
+        if(nextIndex < 10) {
+            try {
+                users[nextIndex++] = user.clone();
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new IndexOutOfBoundsException("no space to add user");
+
+        }
+    }
+
+    @Override
+    protected UserGroup clone() throws CloneNotSupportedException {
+        UserGroup clone = (UserGroup) super.clone();
+        clone.users = users.clone();
+        for (int i = 0; i < nextIndex; i++) {
+            users[i] = users[i].clone();
+        }
+        return clone;
+    }
+}
